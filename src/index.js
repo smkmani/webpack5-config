76ba385a3f2a9da85eda9559e39c0158e2ef1e0b
Sai Mithran <mani.beddy@gmail.com>
2024-07-18 22:20:36 +0530

    Add css sacss postcss

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./styles/index.scss";
 const elvenShieldRecipe = {
   leatherScrips: 2,
   ironIngot: 1,
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,14 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 let mode = "development";
+let target = "web";
+const plugins = [
+  new MiniCssExtractPlugin(),
+  //   new CleanWebpackPlugin(),
+  //   new HtmlWebpackPlugin({
+  //     template: "./src/index.html",
+  //   }),
+];
+
 if (process.env.NODE_ENV === "production") {
   mode = "production";
   // Temporary workaround for 'browserslist' bug that is being patched in the near future
@@ -7,12 +17,23 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
   // mode defaults to 'production' if not set
   mode: mode,
-  devServer: {
-    // contentBase: "./",
-    static: "./",
-  },
   module: {
     rules: [
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            // This is required for asset imports in CSS, such as url()
+            options: { publicPath: "" },
+          },
+          "css-loader",
+          "postcss-loader",
+          // according to the docs, sass-loader should be at the bottom, which
+          // loads it first to avoid prefixes in your sourcemaps and other issues.
+          "sass-loader",
+        ],
+      },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
@@ -23,5 +44,12 @@ module.exports = {
       },
     ],
   },
+  plugins: plugins,
+  devServer: {
+    // contentBase: "./",
+    static: "./dist",
+    hot: true,
+  },
+  target: target,
   devtool: "source-map",
 };
